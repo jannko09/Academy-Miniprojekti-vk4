ad406a4e43cfa386b885eb32095997572d8d600d
codekuki <niko.kuki@kolumbus.fi>
2019-01-29 10:00:32 +0200

    Express-session added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
@@ -17,6 +18,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'miniprojekti1',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    secure: true,
+    maxAge: 60000000               // 100min
+   },
+}));
+
+
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET user's all projects and tasks for specific date or date range */
+router.get('/projects/:date', function(req, res, next) {
+  // Get user id from session info
+  
+  //res.send();
+});
+
+
 module.exports = router;
